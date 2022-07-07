c9f4a341cea50a7721e325d5734daf1708fefd8b
codewithabdur <106903848+codewithabdur@users.noreply.github.com>
2022-07-08 02:10:30 +0530

    pagain

---
+++ build/precache-manifest.d974b4be919252c588ff31d87b2d348f.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "6704372ffc588b925b45",
+    "url": "/static/css/main.25f2f6e5.chunk.css"
+  },
+  {
+    "revision": "6704372ffc588b925b45",
+    "url": "/static/js/main.f9bbb58b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e57997e6f2a0dedde5f",
+    "url": "/static/js/2.91d728d7.chunk.js"
+  },
+  {
+    "revision": "a02ffeea0446038face9dd264d702d47",
+    "url": "/static/media/email.a02ffeea.png"
+  },
+  {
+    "revision": "07a9dce226e6e930853ab55ed1dce0c3",
+    "url": "/static/media/mobile.07a9dce2.png"
+  },
+  {
+    "revision": "8949bfc86fc10ef1505994eb643e940b",
+    "url": "/static/media/cpp.8949bfc8.png"
+  },
+  {
+    "revision": "5c4e16ceb77211d7b286e351e272328d",
+    "url": "/static/media/css.5c4e16ce.png"
+  },
+  {
+    "revision": "338e06da5dc18e11f4b2199aef456cf3",
+    "url": "/static/media/figma.338e06da.png"
+  },
+  {
+    "revision": "9a073b4cb55995549f7dc759eb46f831",
+    "url": "/static/media/git.9a073b4c.png"
+  },
+  {
+    "revision": "1bcf5ba870770831c6b0963c2b796bcb",
+    "url": "/static/media/graphql.1bcf5ba8.png"
+  },
+  {
+    "revision": "19370d004b6acb679a64e08c4dcf3ba0",
+    "url": "/static/media/node.19370d00.png"
+  },
+  {
+    "revision": "c62e7fbf2959f445c0695c7aa460a2a0",
+    "url": "/static/media/redux.c62e7fbf.png"
+  },
+  {
+    "revision": "c88a1a314fa1bc4bcd50b79e21b168a1",
+    "url": "/static/media/sass.c88a1a31.png"
+  },
+  {
+    "revision": "821bcd2bd781e04937c60c5bbdbbaf17",
+    "url": "/static/media/about01.821bcd2b.png"
+  },
+  {
+    "revision": "0b4e9372915752a0d9db5fc50c416263",
+    "url": "/static/media/about02.0b4e9372.png"
+  },
+  {
+    "revision": "7c0e05806ce3588a62fc7f42383c2e48",
+    "url": "/static/media/about03.7c0e0580.png"
+  },
+  {
+    "revision": "6d81249e0a1ce75f945e8b53a7702400",
+    "url": "/static/media/about04.6d81249e.png"
+  },
+  {
+    "revision": "5b9e55f168f2bdec1a0809046c9672d9",
+    "url": "/static/media/profile.5b9e55f1.png"
+  },
+  {
+    "revision": "9300f69fe407f85e9fcd74215502f8b5",
+    "url": "/static/media/circle.9300f69f.svg"
+  },
+  {
+    "revision": "b57cdddc6371fcdea25a4e37b8673915",
+    "url": "/static/media/logo.b57cdddc.png"
+  },
+  {
+    "revision": "79f17773cfd09c7c6a04da0998ad7521",
+    "url": "/static/media/adidas.79f17773.png"
+  },
+  {
+    "revision": "b6dc087f26802781d7b69f624dc68b1c",
+    "url": "/static/media/amazon.b6dc087f.png"
+  },
+  {
+    "revision": "cacd18992c794c7cad00e0f4c7510aa6",
+    "url": "/static/media/asus.cacd1899.png"
+  },
+  {
+    "revision": "d4eab336c0d9c813acc384dad328e240",
+    "url": "/static/media/bolt.d4eab336.png"
+  },
+  {
+    "revision": "df61ee2c437e25eb2124525664552dc5",
+    "url": "/static/media/nb.df61ee2c.png"
+  },
+  {
+    "revision": "e99b220a2081b5bc624a4240aab1e3b8",
+    "url": "/static/media/skype.e99b220a.png"
+  },
+  {
+    "revision": "9cc17d1bf96c54082bcf8e8208c001f2",
+    "url": "/static/media/spotify.9cc17d1b.png"
+  },
+  {
+    "revision": "df093b364e58bd1a14d2f1cfec122e76",
+    "url": "/static/media/bgWhite.df093b36.png"
+  },
+  {
+    "revision": "a6b819e7d95b900cde1602932e3260d9",
+    "url": "/static/media/bgIMG.a6b819e7.png"
+  },
+  {
+    "revision": "46b78b49bf9159a6cc92aad75e00a266",
+    "url": "/index.html"
+  }
+];
